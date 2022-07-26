f4a2fb9192a6c4f9f9ec17b7db521e7c66527441
georgespicy <kolawolegeorge28743@gmail.com>
2022-07-26 11:00:10 +0100

    added paginator to the car page

--- car/views.py
+++ car/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Car
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
+
 def toyota(request):
     cars = Car.objects.all()
+    p = Paginator(cars, 2)
+    page = request.GET.get('page')
+    car_Pages = p.get_page(page)
+    
     context = {
-        'cars': cars,
+        'cars': car_Pages,
     }
     return render (request, 'cars/toyota.html', context)
